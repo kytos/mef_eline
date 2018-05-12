aa3631ebbeede1a3992fb669982412a9d9d80062
Vinicius Arcanjo <viniciusarcanjov@gmail.com>
2018-05-12 14:15:45 -0300

    Added namespace variable on settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 """Settings for the mef_eline NApp."""
+# Namespace
+NAMESPACE = 'kytos.mef_eline.circuits'
 
 # Base URL of the Pathfinder endpoint
 PATHFINDER_URL = 'http://localhost:8181/api/kytos/pathfinder/v2/'
